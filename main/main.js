b36ec7402bede396bf40d873fd8f86af568f260b
DESKTOP-NLMFL51\ZN <2205567926@qq.com>
2017-06-01 09:32:46 +0800

    no message

--- main/main.js
+++ main/main.js
@@ -1,11 +1,13 @@
 // Write your cade below:
 module.exports = function main() {
-  var firstnum=9,firstnum2=9;
-  var secondnum=4,secondnum2=3;
-  var result1,result2;
-  result1=firstnum % secondnum;
-  result2=firstnum2 % secondnum2;
-  console.log("The first reminder is "+result1);
-  console.log("The second reminder is "+result2);
+  var first=9, firsts=9;
+var second=4,seconds=3;
+var result,results;
+result=first%second;
+results=firsts % seconds;
+document.write("The first result is  ",result);
+document.write("<br />");
+document.write("The second result is  ",results);
+
   
 };
