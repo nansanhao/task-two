f6a0609ca69221fcf902627467bc94340938f45d
DESKTOP-NLMFL51\ZN <2205567926@qq.com>
2017-06-01 09:51:41 +0800

    no message

--- main/main.js
+++ main/main.js
@@ -1,13 +1,11 @@
 // Write your cade below:
 module.exports = function main() {
-  var first=9, firsts=9;
-var second=4,seconds=3;
+  var first=9， second=4；
 var result,results;
 result=first % second;
-results=firsts % seconds;
-document.write("The first result is  ",result);
-document.write("<br />");
-document.write("The second result is  ",results);
+console.log("The reminder of 9%4 is 1");
+return result;
+
 
   
 };
