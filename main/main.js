f7da9b935e0d27518c5ba83e0ab467026082642d
DESKTOP-NLMFL51\ZN <2205567926@qq.com>
2017-06-01 09:09:38 +0800

    no message

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 // Write your cade below:
 module.exports = function main() {
+  var firstnum=9,firstnum2=9;
+  var secondnum=4,secondnum2=3;
+  var result1,result2;
+  result1=firstnum % secondnum;
+  result2=firstnum2 % secondnum2;
+  cosole.log("The first reminder is "+result1);
+  cosole.log("The second reminder is "+result2);
   
 };
