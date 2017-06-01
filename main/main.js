f5592392f8136abf229639a6b82befdfb40e2a10
赵楠 <2205567926@qq.com>
2017-06-01 09:52:39 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -3,7 +3,7 @@ module.exports = function main() {
   var first=9， second=4；
 var result,results;
 result=first % second;
-console.log("The reminder of 9%4 is 1");
+console.log("The reminder of 9 % 4 is 1");
 return result;
 
 
