e171e82c8a97b71029de139010e23c457f831eb9
DESKTOP-NLMFL51\ZN <2205567926@qq.com>
2017-06-01 16:16:51 +0800

    no message

--- main/main.js
+++ main/main.js
@@ -1,11 +1,7 @@
 // Write your cade below:
-module.exports = function main() {
-  var first=9， second=4；
-var result,results;
-result=first % second;
-console.log("The reminder of 9 % 4 is 1");
-return result;
-
-
+module.exports = function main(a,b) {
+ if (typeof(a)=="numble" && typeof (b)=="numble"){
+      var result=a%b;}
+   return result;
   
 };
