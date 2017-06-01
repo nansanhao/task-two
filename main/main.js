5dd1498372ac0dd0148bcb033c438ba0746a86a1
DESKTOP-NLMFL51\ZN <2205567926@qq.com>
2017-06-01 09:15:53 +0800

    no message

--- main/main.js
+++ main/main.js
@@ -5,7 +5,7 @@ module.exports = function main() {
   var result1,result2;
   result1=firstnum % secondnum;
   result2=firstnum2 % secondnum2;
-  cosole.log("The first reminder is "+result1);
-  cosole.log("The second reminder is "+result2);
+  console.log("The first reminder is "+result1);
+  console.log("The second reminder is "+result2);
   
 };
